df66f58664022814c6a265f39052c84b59ce80c5
Karolina <cynk.karolina@gmail.com>
2018-11-10 23:47:12 +0100

    Fixed minor bugs in ai

--- ai.pl
+++ ai.pl
@@ -9,14 +9,14 @@
 depth(4).
 
 % List of IDs of all pits on the board
-find_best_move(PlayerBoard, board(OpponentPits, OpponentHouse), BestPit) :-
+find_best_move(board(PlayerPits, PlayerHouse), OpponentBoard, BestPit) :-
   boardSize(BoardSize),
   consecutive(BoardSize, InitialList),
-  all_available_pits(InitialList, OpponentPits, [], CorrectPits),
+  all_available_pits(InitialList, PlayerPits, [], CorrectPits),
   depth(Depth),
   % CorrectPits - pits that contain any seeds. Thus, they can be selected in a moves
-  select_best_move_from_list(CorrectPits, PlayerBoard,
-    board(OpponentPits, OpponentHouse), Depth, minimize, -1, -9999, BestPit, BestValue).
+  select_best_move_from_list(CorrectPits, board(PlayerPits, PlayerHouse), OpponentBoard,
+    Depth, minimize, -1, -9999, BestPit, BestValue).
 
 % all_available_pits([IDs of all pits], OpponentPits, SelectedPits, ResultPits)
 % Pits contains a list of all available pits, that can be chosen by the bot
@@ -31,7 +31,7 @@ all_available_pits([L|Ls], [Z|Zs], ResultPits, FinalPits) :-
   all_available_pits(Ls, Zs, ResultPits, FinalPits).
 
 % Function that analyzes all possible moves made by selecting the pits from the lists
-select_best_move_from_list([], _, _, _, _, Pit, Value, Pit, Value).
+select_best_move_from_list([], PlayerBoard, OpponentBoard, Depth, Type, Pit, Value, Pit, Value).
 
 select_best_move_from_list([Pit|Pits], PlayerBoard, OpponentBoard, Depth, Type,
   CurrentPit, CurrentValue, BestPit, BestValue) :-
@@ -50,6 +50,7 @@ select_best_move_from_list([Pit|Pits], PlayerBoard, OpponentBoard, Depth, Type,
   CurrentPit, CurrentValue, BestPit, BestValue) :-
     % Simulate the move
     seeds_number(Pit, PlayerBoard, SeedsNumber),
+
     % Check if the move would end in players house - if so, apply another move (if game not over)
     sow_seeds(Pit, SeedsNumber, PlayerBoard, OpponentBoard, NewPlayerBoard, NewOpponentBoard),
     !,
@@ -63,23 +64,27 @@ select_best_move_from_list([Pit|Pits], PlayerBoard, OpponentBoard, Depth, Type,
 switch_type(minimize, maximize).
 switch_type(maximize, minimize).
 
-% In case of Depth == 0
-minimax(PlayerBoard, OpponentBoard, 0, maximize, NextType, Pit, Value) :-
-  evaluate(PlayerBoard, OpponentBoard, Val),
-  Value is Val.
-minimax(PlayerBoard, OpponentBoard, 0, minimize, NextType, Pit, Value) :-
-  evaluate(PlayerBoard, OpponentBoard, Val),
-  Value is (-1) * Val.
 
-minimax(PlayerBoard, board(OpponentPits, OpponentHouse), Depth, Type, NextType, Pit, Value) :-
+minimax(board(PlayerPits, PlayerHouse), OpponentBoard, Depth, Type, NextType, Pit, Value) :-
   Depth > 0,
   % Get the list of currently available pits
   boardSize(BoardSize),
   consecutive(BoardSize, InitialList),
-  all_available_pits(InitialList, OpponentPits, [], CorrectPits),
+  all_available_pits(InitialList, PlayerPits, [], CorrectPits),
+  % Prove that list is not empty
+  nth0(0, CorrectPits, _),
   Depth1 is Depth - 1,
-  select_best_move_from_list(CorrectPits, PlayerBoard, board(OpponentPits, OpponentHouse),
+  select_best_move_from_list(CorrectPits, board(PlayerPits, PlayerHouse), OpponentBoard,
   Depth1, NextType, -1, -9999, Pit, Value).
+
+  % In case of Depth == 0 or the CorrectPits list is empty
+minimax(PlayerBoard, OpponentBoard, Depth, maximize, NextType, Pit, Value) :-
+  evaluate(PlayerBoard, OpponentBoard, Val),
+  Value is Val.
+
+minimax(PlayerBoard, OpponentBoard, Depth, minimize, NextType, Pit, Value) :-
+  evaluate(PlayerBoard, OpponentBoard, Val),
+    Value is (-1) * Val.
 
 evaluate(board(Pits, House), board(Pits2, House2), Value) :-
   sum_list(Pits, PitsSum),
--- main.pl
+++ main.pl
@@ -2,9 +2,6 @@
 :- use_module(io_functions).
 :- use_module(ai).
 
-
-% TODO: moduły, stała 6
-
 play :- initialize(PlayerBoard, OpponentBoard, CurrentPlayer),
         play(PlayerBoard, OpponentBoard, CurrentPlayer).
 
