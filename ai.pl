d8a9293b4b1a027efdc6f1a8b344e9dbb09e8075
Karolina <cynk.karolina@gmail.com>
2018-11-12 11:44:14 +0100

    added parameters to evaluate function in ai

--- ai.pl
+++ ai.pl
@@ -2,14 +2,14 @@
 * Functions used for the pit selection, done by the bot.
 */
 
-:- module(ai, [find_best_move/2]).
+:- module(ai, [find_best_move/4]).
 :- use_module(basic_rules).
 :- use_module(utils).
 
-depth(4).
+depth(5).
 
 % List of IDs of all pits on the board
-find_best_move(GameState, BestPit) :-
+find_best_move(GameState, BestPit, HousesWeight, BoardsWeight) :-
   GameState = game_state(board(PlayerPits, _), _, _),
   boardSize(BoardSize),
   consecutive(BoardSize, InitialList),
@@ -17,7 +17,7 @@ find_best_move(GameState, BestPit) :-
   depth(Depth),
   % CorrectPits - pits that contain any seeds. Thus, they can be selected in a moves
   select_best_move_from_list(CorrectPits, GameState,
-    Depth, minimize, -1, -9999, BestPit, _).
+    Depth, minimize, -1, -9999, BestPit, _, HousesWeight, BoardsWeight).
 
 % all_available_pits([IDs of all pits], OpponentPits, SelectedPits, ResultPits)
 % Pits contains a list of all available pits, that can be chosen by the bot
@@ -33,10 +33,10 @@ all_available_pits([_|Ls], [_|Zs], ResultPits, FinalPits) :-
   all_available_pits(Ls, Zs, ResultPits, FinalPits).
 
 % Function that analyzes all possible moves made by selecting the pits from the lists
-select_best_move_from_list([], _, _, _, Pit, Value, Pit, Value).
+select_best_move_from_list([], _, _, _, Pit, Value, Pit, Value, _, _).
 
 select_best_move_from_list([Pit|Pits], GameState0, Depth, Type,
-  CurrentPit0, CurrentValue0, BestPit, BestValue) :-
+  CurrentPit0, CurrentValue0, BestPit, BestValue, HousesWeight, BoardsWeight) :-
     GameState0 = game_state(PlayerBoard, _, _),
     % Simulate the move
     seeds_number(Pit, PlayerBoard, SeedsNumber),
@@ -44,13 +44,13 @@ select_best_move_from_list([Pit|Pits], GameState0, Depth, Type,
     more_turns(Pit, SeedsNumber),
     sow_seeds(Pit, SeedsNumber, GameState0, GameState1),
     !,
-    minimax(GameState1, Depth, Type, Type, _, Value),
+    minimax(GameState1, Depth, Type, Type, _, Value, HousesWeight, BoardsWeight),
     compare(Pit, Value, CurrentPit0, CurrentValue0, CurrentPit1, CurrentValue1),
     select_best_move_from_list(Pits, GameState0, Depth, Type,
-      CurrentPit1, CurrentValue1, BestPit, BestValue).
+      CurrentPit1, CurrentValue1, BestPit, BestValue, HousesWeight, BoardsWeight).
 
 select_best_move_from_list([Pit|Pits], GameState0, Depth, Type,
-  CurrentPit0, CurrentValue0, BestPit, BestValue) :-
+  CurrentPit0, CurrentValue0, BestPit, BestValue, HousesWeight, BoardsWeight) :-
     GameState0 = game_state(PlayerBoard0, _, _),
     GameState1 = game_state(PlayerBoard1, OpponentBoard1, CurrentPlayer1),
     GameState2 = game_state(OpponentBoard1, PlayerBoard1, CurrentPlayer1),
@@ -59,17 +59,17 @@ select_best_move_from_list([Pit|Pits], GameState0, Depth, Type,
     sow_seeds(Pit, SeedsNumber, GameState0, GameState1),
     !,
     switch_type(Type, NextType),
-    minimax(GameState2, Depth, Type, NextType, _, Value),
+    minimax(GameState2, Depth, Type, NextType, _, Value, HousesWeight, BoardsWeight),
     compare(Pit, Value, CurrentPit0, CurrentValue0, CurrentPit1, CurrentValue1),
     select_best_move_from_list(Pits, GameState0, Depth, Type,
-      CurrentPit1, CurrentValue1, BestPit, BestValue).
+      CurrentPit1, CurrentValue1, BestPit, BestValue, HousesWeight, BoardsWeight).
 
 
 switch_type(minimize, maximize).
 switch_type(maximize, minimize).
 
 
-minimax(GameState, Depth, _, NextType, Pit, Value) :-
+minimax(GameState, Depth, _, NextType, Pit, Value, HousesWeight, BoardsWeight) :-
   GameState = game_state(board(PlayerPits, _), _, _),
   Depth > 0,
   % Get the list of currently available pits
@@ -80,22 +80,22 @@ minimax(GameState, Depth, _, NextType, Pit, Value) :-
   nth0(0, CorrectPits, _),
   Depth1 is Depth - 1,
   select_best_move_from_list(CorrectPits, GameState,
-  Depth1, NextType, -1, -9999, Pit, Value).
+  Depth1, NextType, -1, -9999, Pit, Value, HousesWeight, BoardsWeight).
 
   % In case of Depth == 0 or the CorrectPits list is empty
-minimax(GameState, _, maximize, _, _, Value) :-
-  evaluate(GameState, Val),
+minimax(GameState, _, maximize, _, _, Value, HousesWeight, BoardsWeight) :-
+  evaluate(GameState, Val, HousesWeight, BoardsWeight),
   Value is Val.
 
-minimax(GameState, _, minimize, _, _, Value) :-
-  evaluate(GameState, Val),
+minimax(GameState, _, minimize, _, _, Value, HousesWeight, BoardsWeight) :-
+  evaluate(GameState, Val, HousesWeight, BoardsWeight),
     Value is (-1) * Val.
 
-evaluate(GameState, Value) :-
+evaluate(GameState, Value, _, _) :-
   GameState = game_state(board(PlayerPits, PlayerHouse), board(OpponentPits, OpponentHouse), _),
   sum_list(PlayerPits, PlayerPitsSum),
   sum_list(OpponentPits, OpponentPitsSum),
-  Value is 2*(PlayerHouse - OpponentHouse) + 0*(PlayerPitsSum - OpponentPitsSum).
+  Value is 1*(PlayerHouse - OpponentHouse) + 0*(PlayerPitsSum - OpponentPitsSum).
 
 
 % Compares the give Pit and Value with the Currently best Pit and Value
--- main.pl
+++ main.pl
@@ -2,8 +2,11 @@
 :- use_module(io_functions).
 :- use_module(ai).
 
-play :- initialize(GameState),
-        play(GameState).
+play :- play(1, 0).
+
+play(HousesWeight, BoardsWeight) :-
+  initialize(GameState),
+  play(GameState, HousesWeight, BoardsWeight).
 
 % Initialize an empty mancala board with 6 pits (with 4 seeds in each of them) per player
 % side and with 2 empty houses.
@@ -13,30 +16,30 @@ initialize(game_state(board([4,4,4,4,4,4], 0), board([4,4,4,4,4,4], 0), player))
 % 1. Select a start pit to take seeds to sow from.
 % 2. Make the move (sow seeds to consecutive pits). (? Display the board after the move)
 % 3. Switch player and play recursively.
-play(GameState0) :-
-  select_and_make_move(GameState0, GameState1),
+play(GameState0, HousesWeight, BoardsWeight) :-
+  select_and_make_move(GameState0, GameState1, HousesWeight, BoardsWeight),
   switch_player(GameState1, GameState2),
   !,
-  play(GameState2).
+  play(GameState2, HousesWeight, BoardsWeight).
 
-select_and_make_move(GameState0, GameState1) :-
+select_and_make_move(GameState0, GameState1, HousesWeight, BoardsWeight) :-
   display_board(GameState0),
   not(game_over(GameState0)),
-  select_pit(GameState0, Pit),
-  make_move(Pit, GameState0, GameState1).
+  select_pit(GameState0, Pit, HousesWeight, BoardsWeight),
+  make_move(Pit, GameState0, GameState1, HousesWeight, BoardsWeight).
 
 % Selects the pit to move seeds from.
 % Depeding on the CurrentPlayer (whether it is players or computers turn) it waits for
 % the user input (and checks its correctness) or selects the best pit for the move.
-select_pit(GameState, Pit) :-
+select_pit(GameState, Pit, _, _) :-
   GameState = game_state(PlayerBoard, _, player),
   nl, writeln(['Select pit']), read_pit(Pit, seeds_number, PlayerBoard), !.
 
 % A silly strategy: choose the first non-empty pit
-select_pit(GameState, Pit) :-
+select_pit(GameState, Pit, HousesWeight, BoardsWeight) :-
   GameState = game_state(_, _, bot),
   nl, writeln(['Opponent\'s turn']),
-  find_best_move(GameState, Pit),
+  find_best_move(GameState, Pit, HousesWeight, BoardsWeight),
   write('Opponent selected: '),
   writeln(Pit).
 
@@ -46,7 +49,7 @@ select_pit(GameState, Pit) :-
 % NewPlayerBoard, NewOpponentBoard - final state of game board, after distributing
 % the seeds
 % Check if finished in house and whether there should be another move for this player
-make_move(Pit, GameState0, GameState2) :-
+make_move(Pit, GameState0, GameState2, HousesWeight, BoardsWeight) :-
   GameState0 = game_state(PlayerBoard0, _, _),
   seeds_number(Pit, PlayerBoard0, SeedsNumber),
   % Check if the move would end in players house - if so, apply another move (if game not over)
@@ -54,9 +57,9 @@ make_move(Pit, GameState0, GameState2) :-
   writeln('Extra turn!'),
   sow_seeds(Pit, SeedsNumber, GameState0, GameState1),
   !,
-  select_and_make_move(GameState1, GameState2).
+  select_and_make_move(GameState1, GameState2, HousesWeight, BoardsWeight).
 
-make_move(Pit, GameState0, GameState1) :-
+make_move(Pit, GameState0, GameState1, _, _) :-
   GameState0 = game_state(PlayerBoard0, _, _),
   seeds_number(Pit, PlayerBoard0, SeedsNumber),
   sow_seeds(Pit, SeedsNumber, GameState0, GameState1).
